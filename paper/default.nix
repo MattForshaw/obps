18dbc39ee9b6acaaf3258d0d7d88dd629f87c6cd
Valentin Reis <valentin.reis@gmail.com>
2018-01-08 16:16:15 +0100

    some changes

--- paper/default.nix
+++ paper/default.nix
@@ -1,42 +1,6 @@
 with import <nixpkgs> {};
-let
-  myRunLatex =
-  { rootFile
-  , generatePDF ? true # generate PDF, not DVI
-  , generatePS ? false # generate PS in addition to DVI
-  , extraFiles ? []
-  , compressBlanksInIndex ? true
-  , packages ? []
-  , texPackages ? {}
-  , copySources ? false
-}:
-
-assert generatePDF -> !generatePS;
-
-let
-  tex = pkgs.texlive.combine
-  # always include basic stuff you need for LaTeX
-  ({inherit (pkgs.texlive) scheme-basic;} // texPackages);
-in
-
-pkgs.stdenv.mkDerivation {
-  name = "doc";
-
-  builder = ./run-latex.sh;
-  copyIncludes = ./copy-includes.pl;
-
-  inherit rootFile generatePDF generatePS extraFiles
-  compressBlanksInIndex copySources;
-
-  includes = map (x: [x.key (baseNameOf (toString x.key))])
-  (texFunctions.findLaTeXIncludes {inherit rootFile;});
-
-  buildInputs = [ tex pkgs.perl ] ++ packages;
-};
-in
-  myRunLatex {
-    rootFile = src/paper.tex;
-    texPackages = {
+let 
+ texlive = pkgs.texlive.combine {
       inherit (pkgs.texlive)
       everypage
       scheme-full
@@ -60,10 +24,19 @@ in
       trimspaces
       ncctools
       comment
-      dirtree
-      ;
-    };
-    extraFiles = [
-      ./src
-    ];
-  }
+      dirtree ;
+  };
+  callPackage = pkgs.lib.callPackageWith (pkgs);
+in
+stdenv.mkDerivation rec {
+  name="paper";
+  src = ./.;
+  buildPhase = ''
+    make
+  '';
+  installPhase = ''
+    mkdir $out
+    cp paper.pdf $out
+    '';
+  buildInputs = [ texlive pkgs.dia ];
+}
